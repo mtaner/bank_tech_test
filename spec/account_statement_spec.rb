56a136615c0905e673dc40854960974f5d02d6ae
mtaner <mervetaner88@gmail.com>
2016-08-10 14:46:55 +0100

    revise the output for printer test

--- spec/account_statement_spec.rb
+++ spec/account_statement_spec.rb
@@ -8,7 +8,10 @@ describe AccountStatement do
 		it "prints the statement" do
 			account_activity = double("account_activity")
 			allow(account_activity).to receive(:log).and_return [{credit: 1000, balance: 1000, date: '10/08/2016'}, {debit: 100, balance: 900, date: '10/08/2016'}]
-			output = "date             ||     credit(£)    ||      debit(£)    ||    balance(£)\n-------------------------------------------------------------------------\n10/08/2016       ||                  ||       100        ||       900\n10/08/2016       ||      1000        ||                  ||      1000\n"
+			output = "date             ||     credit(£)    ||      debit(£)    ||    balance(£)"\
+			"\n-------------------------------------------------------------------------\n"\
+			"10/08/2016       ||                  ||       100        ||       900\n"\
+			"10/08/2016       ||      1000        ||                  ||      1000\n"
 			expect{account_statement.printer(account_activity.log)}.to output(output).to_stdout
 		end
 	end
