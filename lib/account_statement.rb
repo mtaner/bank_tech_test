183a42d61895a8265b83c442fcfb26aec1f2d95d
mtaner <mervetaner88@gmail.com>
2016-08-10 14:42:15 +0100

    revise printer

--- lib/account_statement.rb
+++ lib/account_statement.rb
@@ -1,12 +1,11 @@
 class AccountStatement
 
 	def printer(statement)
-		puts "date 			|| 	credit 	|| 	debit 	|| 	balance\n------------------------||--------------||--------------||--------------"
+		puts "date" + "%15s" % "||" + "%14s" % "credit(£)" + "%6s" % "||" + "%14s" % "debit(£)" + "%6s" % "||" + "%14s" % "balance(£)"
+		puts "-------------------------------------------------------------------------"
 
 		statement.reverse.each do |hash|
-				puts "#{hash[:date]}		|| 	#{hash[:credit] || ""} 	|| 	#{hash[:debit] || ""} 	|| 	#{hash[:balance]}"
+				puts "#{hash[:date]}" + "%9s" % "||" + "%10s" % "#{hash[:credit] || ""}" + "%10s" % "||" + "%10s" % "#{hash[:debit] || ""}" + "%10s" % "||" + "%10s" % "#{hash[:balance]}"
 		end
-
 	end
-
 end
